8fd11af4dca98b25affc12cf83d16e301468e76a
JamesDorling <jamesrdorling@gmail.com>
2021-12-09 22:45:55 +0000

    Tested the SortFactory

---
+++ src/test/java/org/sort_manager/SortFactoryTests.java
@@ -0,0 +1,38 @@
+package org.sort_manager;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+import org.sort_manager.sort_methods.SortMethod;
+import org.sort_manager.sort_methods.binary_tree.BinaryTree;
+import org.sort_manager.sort_methods.bubble_sort.BubbleSortIntArray;
+
+import java.util.stream.Stream;
+
+public class SortFactoryTests {
+
+    @BeforeAll
+    static void setupAll(TestInfo testInfo) {
+        System.out.println(testInfo.getTestClass() + " executing");
+    }
+
+    @AfterAll
+    static void tearDownAll(TestInfo testInfo) { System.out.println(testInfo.getTestClass() + " has completely finished testing."); }
+
+    public static Stream<Arguments> sortCheckerArguments() {
+        return Stream.of(
+                Arguments.arguments("bubble", new Integer[]{3, 2}, BubbleSortIntArray.class),
+                Arguments.arguments("tree", new Integer[]{3, 2}, BinaryTree.class),
+                Arguments.arguments("bubble", new Integer[]{3, 2}, BubbleSortIntArray.class)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("sortCheckerArguments")
+    @DisplayName("Check if correct sorters are created test")
+    public void makeCorrectSortersTest(String sortType, Integer[] arrayToSort, Class expectedSortMethod) {
+        Assertions.assertEquals(expectedSortMethod, SortFactory.getSorter(sortType, arrayToSort).getClass());
+    }
+
+
+}
