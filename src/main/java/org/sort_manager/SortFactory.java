f2193e4bc4a0e2108b0dc94ff39de59efbd89d3c
JamesDorling <jamesrdorling@gmail.com>
2021-12-09 19:04:41 +0000

    Made the factory abstract class.

---
+++ src/main/java/org/sort_manager/SortFactory.java
@@ -0,0 +1,20 @@
+package org.sort_manager;
+
+import org.sort_manager.sort_methods.SortMethod;
+import org.sort_manager.sort_methods.binary_tree.BinaryTree;
+import org.sort_manager.sort_methods.bubble_sort.BubbleSortIntArray;
+
+public class SortFactory {
+    public SortMethod getSorter(String sortType, Integer[] arrayToSort) {
+        return switch (sortType) {
+            case "bubble" -> new BubbleSortIntArray(arrayToSort);
+            case "tree" -> new BinaryTree(arrayToSort);
+            case "merge" -> null;
+            case "insertion" -> null;
+            default ->
+                    //return null, instructors aint liking this one but its temporary so please dont kill me
+                    null;
+            //Do all of them? Why not?
+        };
+    }
+}
